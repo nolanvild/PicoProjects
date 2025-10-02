f85fceac5b9d868097c22d47c096424a30a37e73
nolanvild <149283286+nolanvild@users.noreply.github.com>
2025-10-02 00:51:37 -0400

    Save Data into one json file

--- Sports Data Fetching.py
+++ Sports Data Fetching.py
@@ -1,9 +1,11 @@
 import network
 import urequests
 import machine
+import ntptime
 import time
 import json
 import requests
+import os
 from secrets import SportsDataIO_API_KEY, ssid, password
 
 
@@ -22,23 +24,27 @@ def connect_to_wifi(ssid, password):
     print('Network config:', wlan.ifconfig())
     return wlan
 
+
 wlan = connect_to_wifi(ssid, password)
 
-print(f'{wlan.isconnected()}')
+# Set the time for game checks
+def get_eastern_time():
+    ntptime.host = 'pool.ntp.org'
+    ntptime.settime()
+    rtc = machine.RTC()
+    current_time = rtc.datetime()
+    eastern_time = list(current_time)
+    eastern_time[4] = (eastern_time[4] - 4 )%24
+    return eastern_time
+eastern_time = get_eastern_time()
+print(f"The current time is: {eastern_time}")
 
-# Example 2. urequests can also handle basic json support! Let's get the curren time from a server
-r = urequests.get("http://example.com")
-data = r
-print("Success! Length:", len(r.text))
 
-filename = "data.txt"
-with open(filename, "w") as f:
-    f.write(data.text)
     
 def lookup_scores(teams, entries, week, year):
-    print(week)
+    # Box score - by Team [Final]
     url = f"https://api.sportsdata.io/v3/nfl/stats/json/BoxScoreByTeamFinal/{year}/{week}/{team}?key={SportsDataIO_API_KEY}"
-    response = urequests.get(url)
+    response = urequests.get(url)   
 
     # Stream through the response text to find the HomeTeam
     buffer = ""
@@ -49,6 +55,7 @@ def lookup_scores(teams, entries, week, year):
     for entry in entries:
         chunk = response.raw.read(256)  # read 256 bytes at a time
         if not chunk:
+            print("No chunk to read")
             break
         buffer += chunk.decode()
 
@@ -60,16 +67,37 @@ def lookup_scores(teams, entries, week, year):
             end_quote = buffer.find(',', start_quote)
             data = buffer[start_quote:end_quote]
             output[f"{entry}"] = data[1:]
-                
+    response.close()
     return output
     
 # Test API Call
 team = "PIT"
-week = 1
+weeks = [1, 2, 3, 4, 5, 6, 7]
 year = 2025
 entries = ["AwayTeam", "HomeTeam", "AwayScore", "HomeScore"]
-output_data = lookup_scores(team, entries, week, year)
+json_filename = f"{team}_year{year}.json"
 
-# Print API Data
-for k, v in output_data.items():
-    print(f"{k}: {v}")
+for week in weeks:
+    output_data = lookup_scores(team, entries, week, year)
+
+    try:
+        os.stat(json_filename)
+        file_exists = True
+    except OSError:
+        file_exists = False
+    
+    if not file_exists:
+        file_data = {}
+    else:
+        with open(json_filename, 'r') as f:
+            file_data = json.load(f)
+            file_data[f"{week}"] = output_data
+        
+    with open(json_filename, 'w') as f:
+        json.dump(file_data, f)
+            
+with open(json_filename, 'r') as f:
+    data = json.load(f)
+    for k, v in data.items():
+        print(f"{k}: {v}")
+    
